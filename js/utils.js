cf5c61280be5ab4bc61b3f1e901e6221cc6ae744
Jorge Díaz <jorgediaz9515@gmail.com>
2022-06-07 18:40:42 -0600

    commit 03
    consumo del JSON y arreglo del template pagina principal

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,8 @@
-//slider
+var app = angular.module("tiendaOnline_DAW2022", []);
+app.controller("renderData", function($scope, $http) {
+  $http.get("productos_libros.json").
+  then(function(response) {
+    $scope.Libros = response.data;
+
+  });
+});
