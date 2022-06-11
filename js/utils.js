1076260bc5f48e6ea7fb8f1924062695005c0ae2
Jorge Díaz <jorgediaz9515@gmail.com>
2022-06-11 15:05:26 -0600

    commit 05
    Añadi modales bootstrap xD YOLO porque con AngularJS y bootstrap hay conflictos por lo que Angular se encargara de otras funciones

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,8 @@
-var app = angular.module("tiendaOnline_DAW2022", []);
-app.controller("renderData", function($scope, $http) {
-  $http.get("productos_libros.json").
-  then(function(response) {
-    $scope.Libros = response.data;
+var app = angular.module("tiendaOnline_DAW2022", ["ngAnimate"]);
 
-  });
-});
+app.controller("renderData", function ($scope, $http) {
+  $http.get("productos_libros.json").
+    then(function (response) {
+      $scope.Libros = response.data;
+    }); 
+ });
